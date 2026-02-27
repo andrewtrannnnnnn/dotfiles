d413391bdf525f85c199efb31e26ea408dff6829
Andrew Tran <andrewtrann233@gmail.com>
2026-02-27 11:03:23 -0800

    feature(theme): add catppuccin theme

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make"
+      }
+    },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>" }
+    },
+    opts = {}
+  }
+}
